22862d82165b360d8bb00731c9f57794e72acbcb
Shegzzy <miksegun1996@gmail.com>
2023-05-20 00:51:53 +0100

    requirement file

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -68,10 +68,21 @@ WSGI_APPLICATION = "ecommerce.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "WEPqrbR5rvTkAHHD18k1",
+        "HOST": "containers-us-west-39.railway.app",
+        "PORT": "7670",
     }
 }
 
