d13724b849f4084023e3a6f9d7e79aaa1a24202b
Shegzzy <miksegun1996@gmail.com>
2023-05-20 09:29:19 +0100

    requirement

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = "(8e6mxc1cw4o--_k2*8(@mk-0&hw^p-=&ga&59=t724u3g3tqj"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://denpoly-store-production.up.railway.app/", "localhost"]
+ALLOWED_HOSTS = ["denpoly-store-production.up.railway.app/", "localhost"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
